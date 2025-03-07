5bab9b9cf90b3f781f10d5efaf39315ff63e3642
uciboy14 <daviduciboy@gmail.com>
2025-03-07 18:00:19 -0500

    Commit all

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import About from "@/components/About";
+import About from "../components/About";
 
 export default function Home() {
   return (
-    <div className="bg-gray-900 min-h-screen">
+    <div className="bg-gray-900">
       <Navbar />
       <Hero />
       <About />
--- components/About.js
+++ components/About.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import PersonalInfo from "./PersonalInfo";
 
 const About = () => {
+  const personalDetails = [
+    { label: "Birthday", value: "9 March 1999" },
+    { label: "Website", value: "www.uccodetech.com" },
+    { label: "Phone", value: "+2349063324523" },
+    { label: "City", value: "Lagos, Nigeria" },
+    { label: "Age", value: "26" },
+    { label: "Degree", value: "Bsc." },
+    { label: "Mail", value: "uccodetech@gmail.com" },
+    { label: "Freelance", value: "Available" },
+  ];
+
+  const firstHalf = personalDetails.slice(0, 4);
+  const secondHalf = personalDetails.slice(4, 8);
+
   return (
-    <div>About</div>
-  )
-}
+    <section
+      className="bg-gray-900 flex items-center justify-center text-[#fafafa] font-light leading-6 py-[100px] pb-[48px] text-left
+"
+    >
+      <div className="max-w-7xl mx-auto px-8 ">
+        <div className="p-8 rounded-lg flex flex-col md:flex-row items-center">
+          {/* Image Section */}
+          <div
+            className="text-[#fafafa] basis-1/2 font-light leading-6 px-[15px] text-left
+"
+          >
+            <img
+              src="/about.jpg"
+              alt="Workspace"
+              className="rounded-lg shadow-lg w-full"
+            />
+          </div>
+
+          {/* Text Section */}
+          <div
+            className="text-[#fafafa] basis-1/2 font-light leading-6 px-[15px] text-left
+"
+          >
+            <div className="personal-items">
+              <h2 className="base-color text-lg">About Me</h2>
+              <h5 className="poppins text-[20px] leading-[32.36px] font-medium text-white my-4 text-left">
+                <span className="base-color inline-block">Full-Stack </span> &
+                Flutter Mobile Developer
+              </h5>
+              <div className="flex">
+                <PersonalInfo items={firstHalf} />
+                <PersonalInfo items={secondHalf} />
+              </div>
+            </div>
+
+            {/* Buttons */}
+            <div className="mt-6 flex gap-4">
+              <button className="muli text-[16px] leading-6 text-left tracking-normal text-[#fafafa] px-[30px] py-[10px] bg-[#1ab394] hover:bg-[#0c9b7e] transition rounded-[16px] shadow-none">
+                Download CV
+              </button>
+              <button className="muli text-[16px] leading-6 text-left tracking-normal text-[#fafafa] px-[30px] py-[10px] bg-[#1ab394] hover:bg-[#0c9b7e] transition rounded-[16px] shadow-none">
+                Hire Me
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
