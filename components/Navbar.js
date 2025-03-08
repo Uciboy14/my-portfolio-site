e5ced9debc3eb18cce56467974cb196435a900f3
uciboy14 <daviduciboy@gmail.com>
2025-03-08 15:44:17 -0500

    Commit all

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,17 +19,31 @@ export default function Navbar() {
         scrolled ? "bg-gray-900 shadow-md" : "bg-transparent"
       }`}
     >
-      <div className="container mx-auto flex justify-between items-center p-5">
-        <h1 className="text-green-500 text-xl font-bold">UccodeTech</h1>
-        <ul className="hidden md:flex space-x-6 text-white">
-          {["Home", "About", "Services", "Portfolio", "Client", "Blog", "Contact"].map(
-            (item, index) => (
-              <li key={index} className="hover:text-green-400 cursor-pointer text-[17px]">
-                {item}
-              </li>
-            )
-          )}
-        </ul>
+      <div className="text-[#fafafa] font-light leading-6 px-0 py-2 text-left muli">
+        <div className="flex items-center justify-start text-[#fafafa] font-light leading-6 px-4 py-2 text-left muli">
+          <a className="text-green-500 text-xl font-bold text-[22px] leading-[33px] mx-[7px] py-[5px] text-left"
+>UccodeTech</a>
+          <div className="flex items-end justify-end w-full text-[#fafafa] font-light leading-6 text-left">
+            <ul className="hidden md:flex space-x-6 text-white">
+              {[
+                "Home",
+                "About",
+                "Services",
+                "Portfolio",
+                "Client",
+                "Blog",
+                "Contact",
+              ].map((item, index) => (
+                <li
+                  key={index}
+                  className="nav-link"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
